b131f340261f624945f7d196cd1f33870c5b292b
tejasmishra1701 <tejasmishralar.3017@gmail.com>
2025-07-18 12:27:05 +0530

    04- background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,23 @@
 import { z } from 'zod';
+
+import { inngest } from '@/inngest/client';
 import { baseProcedure, createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
+  invoke: baseProcedure
+    .input(
+      z.object({
+        text: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { text } = input;
+      // Call the Inngest function with the text
+      const response = await inngest.send({
+        name: 'test/hello.world2',
+        data: { email: input.text },
+      });
+      return response;
+    }),
   createAI: baseProcedure
     .input(
       z.object({
